9c58537815896a6467810578d0b01edb9f1ca7d0
hungquangnguyen <hung.nguyen.jx@renesas.com>
2023-02-23 12:56:23 +0700

    Enable drop

--- lua/user/plugins/drop.lua
+++ lua/user/plugins/drop.lua
@@ -5,9 +5,9 @@ end
 
 local options = {
     theme = "leaves", -- can be one of rhe default themes, or a custom theme
-    max = 40, -- maximum number of drops on the screen
-    interval = 10, -- every 150ms we update the drops
-    screensaver = 1000 * 60 * 5, -- show after 5 minutes. Set to false, to disable
+    max = 20, -- maximum number of drops on the screen
+    interval = 20, -- every 150ms we update the drops
+    screensaver = 1000 * 60 * 10, -- show after 5 minutes. Set to false, to disable
     filetypes = { "dashboard", "alpha", "starter" }, -- will enable/disable automatically for the following filetypes
 }
 
