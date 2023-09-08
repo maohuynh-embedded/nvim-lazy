8ed01a944b1db0a3f422243cc2c87893bd2ee0bd
unknown <quanghung1599@gmail.com>
2023-09-08 23:26:25 +0700

    Update config for jabs

--- lua/user/plugins/jabs.lua
+++ lua/user/plugins/jabs.lua
@@ -4,65 +4,53 @@ if not status_ok then
     return
 end
 
-local options = {
+jabs.setup({
     -- Options for the main window
     position = 'corner', -- center, corner. Default corner
-    width = 55, -- default 50
-    height = 10, -- default 10
-    border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
-
-    offset = { -- window position offset
-        top = 2, -- default 0
-        bottom = 2, -- default 0
-        left = 2, -- default 0
-        right = 2, -- default 0
+    width = 50,          -- default 50
+    height = 10,         -- default 10
+    border = 'rounded',  -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
+    offset = {
+                         -- window position offset
+        top = 2,         -- default 0
+        bottom = 2,      -- default 0
+        left = 2,        -- default 0
+        right = 2,       -- default 0
     },
-
     -- Options for preview window
-    preview_position = 'top', -- top, bottom, left, right. Default top
+    preview_position = 'left', -- top, bottom, left, right. Default top
     preview = {
-        width = 70, -- default 70
-        height = 30, -- default 30
-        border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
+        width = 100,           -- default 70
+        height = 30,           -- default 30
+        border = 'rounded',    -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
     },
-
-    sort_mru = true, -- Sort buffers by most recently used (true or false). Default false
-    split_filename = true, -- Split filename into separate components for name and path. Default false
-    split_filename_path_width = 20, -- If split_filename is true, how wide the column for the path is supposed 
-
     -- Default highlights (must be a valid :highlight)
     highlight = {
-        current = "StatusLine", -- default StatusLine
-        hidden = "ModeMsg", -- default ModeMsg
-        split = "StatusLine", -- default StatusLine
-        alternate = "WarningMsg" -- default WarningMsg
+        current = "Title",       -- default StatusLine
+        hidden = "StatusLineNC", -- default ModeMsg
+        split = "WarningMsg",    -- default StatusLine
+        alternate = "StatusLine" -- default WarningMsg
     },
-
     -- Default symbols
-    -- symbols = {
-    --     current = "C", -- default 
-    --     split = "S", -- default 
-    --     alternate = "A", -- default 
-    --     hidden = "H", -- default ﬘
-    --     locked = "L", -- default 
-    --     ro = "R", -- default 
-    --     edited = "E", -- default 
-    --     terminal = "T", -- default 
-    --     default_file = "D", -- Filetype icon if not present in nvim-web-devicons. Default 
-    -- },
-
+    symbols = {
+        current = "󰽏",
+        split = "",
+        alternate = "󰬲",
+        hidden = "󰌨",
+        locked = "",
+        ro = "",
+        edited = "",
+        terminal = "",
+        default_file = "",
+    },
     -- Keymaps
     keymap = {
-        close = "d", -- Close buffer. Default D
-        jump = "<CR>", -- Jump to buffer. Default <cr>
-        h_split = "h", -- Horizontally split buffer. Default s
-        v_split = "v", -- Vertically split buffer. Default v
+        close = "d",         -- Close buffer. Default D
+        jump = "<cr>",       -- Jump to buffer. Default <cr>
+        h_split = "h",       -- Horizontally split buffer. Default s
+        v_split = "v",       -- Vertically split buffer. Default v
         preview = "<space>", -- Open buffer preview. Default P
     },
-
     -- Whether to use nvim-web-devicons next to filenames
     use_devicons = true -- true or false. Default true
-}
-
-jabs.setup(options)
--- vim.keymap.set("n", "t", ":JABSOpen<CR>", { noremap = true, silent = true })
+})
