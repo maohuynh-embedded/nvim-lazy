d245ca89bdf92dd4ec4fb87af3ae8210ae1bbd55
Hung Nguyen RVC <quanghung1599@gmail.com>
2023-09-26 23:35:52 +0700

    Update config for neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -77,12 +77,12 @@ end
 -- neovide
 if vim.g.neovide then
     -- Put anything you want to happen only in Neovide here
-vim.g.neovide_transparency = 1
-vim.g.neovide_refresh_rate = 60
+    vim.g.neovide_transparency = 1
+    vim.g.neovide_refresh_rate = 60
     vim.g.neovide_refresh_rate_idle = 60
     vim.g.neovide_confirm_quit = true
     vim.g.neovide_touch_drag_timeout = 0.17
-    vim.g.neovide_cursor_antialiasing = false
+    vim.g.neovide_cursor_antialiasing = true
     vim.g.neovide_scroll_animation_length = 0.3
     vim.g.neovide_cursor_vfx_particle_density = 0.2
     vim.g.neovide_hide_mouse_when_typing = true
@@ -94,11 +94,11 @@ vim.g.neovide_refresh_rate = 60
     vim.g.neovide_floating_blur_amount_x = 0.0
     vim.g.neovide_floating_blur_amount_y = 0.0
     --
-    -- vim.g.neovide_cursor_unfocused_outline_width = 0.125
+    vim.g.neovide_cursor_unfocused_outline_width = 0.125
     -- global.neovide_cursor_vfx_particle_lifetime = 1.2
 
     -- vim.g.neovide_cursor_vfx_particle_curl = 1.0
-    -- vim.g.neovide_cursor_vfx_particle_density = 7.0
+    vim.g.neovide_cursor_vfx_particle_density = 0
     -- vim.g.neovide_cursor_vfx_particle_speed = 10.0
     -- vim.g.neovide_cursor_vfx_particle_phase = 1.5
     -- vim.g.neovide_cursor_vfx_mode = "railgun"
