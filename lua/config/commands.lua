fa45e1860bd50191dce2a33f39a07e2da3821eb7
hungquangnguyen <hung.nguyen.jx@renesas.com>
2024-02-02 14:46:26 +0700

    Fix color issue in Winbar

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -105,13 +105,6 @@ autocmd("User", {
     end,
 })
 
-vim.api.nvim_set_hl(0, 'WinbarNC', { link = 'WinBar' })
-
-vim.api.nvim_set_hl(0, 'IblScope', { ctermbg = 0, fg = '#fca7ca' })
-vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939' })
-vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef' })
-vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379' })
-
 vim.fn.sign_define('DapBreakpoint', { text = ' ', texthl = 'DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointCondition', { text = ' ', texthl = 'DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointRejected', { text = ' ', texthl = 'DapBreakpoint' })
--- lua/user/plugins/tokyonight.lua
+++ lua/user/plugins/tokyonight.lua
@@ -6,9 +6,9 @@ end
 tokyonight.setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-    light_style = "day", -- The theme is used when the background is set to light
-    transparent = false, -- Enable this to disable setting the background color
+    style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    light_style = "day",    -- The theme is used when the background is set to light
+    transparent = false,    -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
         -- Style to be applied to different syntax groups
@@ -19,14 +19,14 @@ tokyonight.setup({
         variables = { fg = '#f9f2eb' },
         -- macro = { fg = '#57afef'},
         -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "moon", -- style for sidebars, see below
-        floats = "storm", -- style for floating windows
+        sidebars = "moon",           -- style for sidebars, see below
+        floats = "storm",            -- style for floating windows
     },
-    sidebars = {}, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    sidebars = {},                   -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+    day_brightness = 0.3,            -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = true, -- dims inactive windows
-    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+    dim_inactive = true,             -- dims inactive windows
+    lualine_bold = true,             -- When `true`, section headers in the lualine theme will be bold
 
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
@@ -34,7 +34,32 @@ tokyonight.setup({
 
     --- You can override specific highlights to use other groups or a hex color
     --- function will be called with a Highlights and ColorScheme table
-    on_highlights = function(highlights, colors) end,
+    on_highlights = function(highlights, colors)
+        highlights.Winbar = {
+            ctermbg = 0,
+            bg = '#222436'
+        }
+        highlights.WinbarNC = {
+            ctermbg = 0,
+            bg = '#222436'
+        }
+        highlights.IblScope = {
+            ctermbg = 0,
+            fg = '#fca7ca'
+        }
+        highlights.DapBreakpoint = {
+            ctermbg = 0,
+            fg = '#993939'
+        }
+        highlights.DapLogPoint = {
+            ctermbg = 0,
+            fg = '#61afef'
+        }
+        highlights.DapStopped = {
+            ctermbg = 0,
+            fg = '#98c379'
+        }
+    end,
 })
 
 tokyonight.load()
