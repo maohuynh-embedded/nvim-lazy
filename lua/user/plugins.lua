9a1429a4498b41c668ed048085e00786df557998
hungquangnguyen <hung.nguyen.jx@renesas.com>
2024-02-15 14:03:14 +0700

    Test markdown preview

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -847,5 +847,17 @@ return {
         init = function()
             require("user.plugins.gesture")
         end
-    }
+    },
+
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup()
+            -- refer to `configuration to change defaults`
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    },
 }
