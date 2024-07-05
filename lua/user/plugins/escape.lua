98910330eee01ccca43bb42659764b868239c021
hungquangnguyen <hung.nguyen.jx@renesas.com>
2024-07-05 11:12:21 +0700

    Update the escape plugin

--- lua/user/plugins/escape.lua
+++ lua/user/plugins/escape.lua
@@ -1,19 +1,43 @@
 local status_ok, better_escape = pcall(require, "better_escape")
 if not status_ok then
-    print("Error: dropbar")
+    print("Error: better_escape")
     return
 end
 
 -- lua, default settings
 local options = {
-    mapping = { "jk", "kj" }, -- a table with mappings to use
-    timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-    -- example(recommended)
-    -- keys = function()
-    --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-    -- end,
+    timeout = vim.o.timeoutlen,
+    mappings = {
+        i = {
+            j = {
+                -- These can all also be functions
+                k = "<Esc>",
+                j = "<Esc>",
+            },
+        },
+        c = {
+            j = {
+                k = "<Esc>",
+                j = "<Esc>",
+            },
+        },
+        t = {
+            j = {
+                k = "<Esc>",
+                j = "<Esc>",
+            },
+        },
+        v = {
+            j = {
+                k = "<Esc>",
+            },
+        },
+        s = {
+            j = {
+                k = "<Esc>",
+            },
+        },
+    }
 }
 
 better_escape.setup(options)
